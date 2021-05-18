df5af6aac3591e9dc001d9e271fd2ae85c9488d4
Sharaz Nabi <sharaz4all@gmail.com>
2021-05-18 13:59:30 +0530

    code fix

--- java-rest-api-demo-submodule/src/main/java/com/example/twilio/demo/SendSMS.java
+++ java-rest-api-demo-submodule/src/main/java/com/example/twilio/demo/SendSMS.java
@@ -11,7 +11,7 @@ public class SendSMS {
 
 	// Find your Account Sid and Auth Token at twilio.com/console
 	 public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-	    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+	 public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
 	/*
 	 * public static void main(String[] args) { Twilio.init(ACCOUNT_SID,
